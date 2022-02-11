aa84144233d5d0bc1882b8e93177a5a307b2ebdc
Zhengxu Xia <kevinxzx95@gmail.com>
2022-02-11 16:29:13 -0600

    Update bellevue driver script.

--- ekya/experiment_drivers/driver_profiling_mp4_golden_bellevue.sh
+++ ekya/experiment_drivers/driver_profiling_mp4_golden_bellevue.sh
@@ -3,12 +3,10 @@
 # retraining results in accuracy gains.
 set -e
 
-# DATASET_NAME=mp4
 DATASET_NAME=bellevue
-DATASET_PATH=/data/zxxia/ekya/datasets/mp4/${DATASET_NAME}
-MODEL_PATH='/data/zxxia/ekya/mp4_saved_models/'
-# MODEL_PATH='/data/zxxia/ekya/bellevue_saved_models/'
-GOLDEN_MODEL_PATH='/data2/zxxia/ekya/golden_model/coco_resnext101_elastic.pth.tar'
+DATASET_PATH=../../dataset/${DATASET_NAME}
+MODEL_PATH=${DATASET_PATH}/pretrained_models
+GOLDEN_MODEL_PATH=../../golden_model/coco_resnext101_elastic.pth.tar
 HYPS_PATH='profiling/hyp_map_all.json'
 INFERENCE_PROFILE_PATH='real_inference_profiles.csv'
 RETRAINING_PERIOD=9999999 # Large number to give enough time to complete. Tasks terminate early when retraining finishes so this is not a problem.
@@ -22,25 +20,52 @@ MAX_INFERENCE_RESOURCES=0.25
 
 # CityScapes cities we are interested in:
 # All possible retraining hyperparameters
-# 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
-
-             # --lists-pretrained las_vegas_10fps_2701_7200 \
-for HPARAM_ID in 0 1 2 3 4 5; do
-
-
-# Bellevue_Bellevue_NE8th__2017-09-10_18-08-23_0.5fps
-
-
-    # for CITY in Bellevue_150th_Eastgate__2017-09-10_18-08-24_0.5fps Bellevue_150th_Eastgate__2017-09-10_21-08-28_0.5fps Bellevue_150th_Eastgate__2017-09-11_00-08-29_0.5fps Bellevue_150th_Eastgate__2017-09-11_03-08-30_0.5fps Bellevue_150th_Eastgate__2017-09-11_06-08-30_0.5fps Bellevue_150th_Eastgate__2017-09-11_09-08-31_0.5fps Bellevue_150th_Eastgate__2017-09-11_14-08-31_0.5fps Bellevue_150th_Eastgate__2017-09-11_15-08-33_0.5fps; do
-    # for CITY in Bellevue_150th_Newport__2017-09-10_18-08-24_0.5fps Bellevue_150th_Newport__2017-09-10_21-08-28_0.5fps Bellevue_150th_Newport__2017-09-11_00-08-29_0.5fps Bellevue_150th_Newport__2017-09-11_03-08-29_0.5fps Bellevue_150th_Newport__2017-09-11_06-08-30_0.5fps Bellevue_150th_Newport__2017-09-11_09-08-30_0.5fps Bellevue_150th_Newport__2017-09-11_12-08-32_0.5fps Bellevue_150th_Newport__2017-09-11_15-08-32_0.5fps; do
-    # for CITY in Bellevue_150th_SE38th__2017-09-10_18-08-24_0.5fps Bellevue_150th_SE38th__2017-09-10_21-08-38_0.5fps Bellevue_150th_SE38th__2017-09-11_00-08-29_0.5fps Bellevue_150th_SE38th__2017-09-11_03-08-30_0.5fps Bellevue_150th_SE38th__2017-09-11_06-08-32_0.5fps Bellevue_150th_SE38th__2017-09-11_09-08-31_0.5fps Bellevue_150th_SE38th__2017-09-11_12-08-38_0.5fps Bellevue_150th_SE38th__2017-09-11_16-08-35_0.5fps; do
-    # for CITY in Bellevue_Bellevue_NE8th__2017-09-10_18-08-23_0.5fps Bellevue_Bellevue_NE8th__2017-09-10_21-08-28_0.5fps Bellevue_Bellevue_NE8th__2017-09-11_00-08-29_0.5fps Bellevue_Bellevue_NE8th__2017-09-11_03-08-29_0.5fps Bellevue_Bellevue_NE8th__2017-09-11_06-08-29_0.5fps Bellevue_Bellevue_NE8th__2017-09-11_09-08-30_0.5fps Bellevue_Bellevue_NE8th__2017-09-11_12-08-31_0.5fps Bellevue_Bellevue_NE8th__2017-09-11_15-08-32_0.5fps; do
-    for CITY in  Bellevue_116th_NE12th__2017-09-10_19-08-25_0.5fps Bellevue_116th_NE12th__2017-09-10_22-08-50_0.5fps Bellevue_116th_NE12th__2017-09-11_01-08-29_0.5fps Bellevue_116th_NE12th__2017-09-11_04-08-30_0.5fps Bellevue_116th_NE12th__2017-09-11_07-08-32_0.5fps Bellevue_116th_NE12th__2017-09-11_11-08-33_0.5fps Bellevue_116th_NE12th__2017-09-11_15-08-36_0.5fps Bellevue_116th_NE12th__2017-09-11_16-08-37_0.5fps; do
+for HPARAM_ID in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17; do
+    # for CITY in Bellevue_150th_Eastgate__2017-09-10_18-08-24_0.5fps \
+    #     Bellevue_150th_Eastgate__2017-09-10_21-08-28_0.5fps \
+    #     Bellevue_150th_Eastgate__2017-09-11_00-08-29_0.5fps \
+    #     Bellevue_150th_Eastgate__2017-09-11_03-08-30_0.5fps \
+    #     Bellevue_150th_Eastgate__2017-09-11_06-08-30_0.5fps \
+    #     Bellevue_150th_Eastgate__2017-09-11_09-08-31_0.5fps \
+    #     Bellevue_150th_Eastgate__2017-09-11_14-08-31_0.5fps \
+    #     Bellevue_150th_Eastgate__2017-09-11_15-08-33_0.5fps; do
+    # for CITY in Bellevue_150th_Newport__2017-09-10_18-08-24_0.5fps \
+    #     Bellevue_150th_Newport__2017-09-10_21-08-28_0.5fps \
+    #     Bellevue_150th_Newport__2017-09-11_00-08-29_0.5fps \
+    #     Bellevue_150th_Newport__2017-09-11_03-08-29_0.5fps \
+    #     Bellevue_150th_Newport__2017-09-11_06-08-30_0.5fps \
+    #     Bellevue_150th_Newport__2017-09-11_09-08-30_0.5fps \
+    #     Bellevue_150th_Newport__2017-09-11_12-08-32_0.5fps \
+    #     Bellevue_150th_Newport__2017-09-11_15-08-32_0.5fps; do
+    # for CITY in Bellevue_150th_SE38th__2017-09-10_18-08-24_0.5fps \
+    #     Bellevue_150th_SE38th__2017-09-10_21-08-38_0.5fps \
+    #     Bellevue_150th_SE38th__2017-09-11_00-08-29_0.5fps \
+    #     Bellevue_150th_SE38th__2017-09-11_03-08-30_0.5fps \
+    #     Bellevue_150th_SE38th__2017-09-11_06-08-32_0.5fps \
+    #     Bellevue_150th_SE38th__2017-09-11_09-08-31_0.5fps \
+    #     Bellevue_150th_SE38th__2017-09-11_12-08-38_0.5fps \
+    #     Bellevue_150th_SE38th__2017-09-11_16-08-35_0.5fps; do
+    # for CITY in Bellevue_Bellevue_NE8th__2017-09-10_18-08-23_0.5fps \
+    #     Bellevue_Bellevue_NE8th__2017-09-10_21-08-28_0.5fps \
+    #     Bellevue_Bellevue_NE8th__2017-09-11_00-08-29_0.5fps \
+    #     Bellevue_Bellevue_NE8th__2017-09-11_03-08-29_0.5fps \
+    #     Bellevue_Bellevue_NE8th__2017-09-11_06-08-29_0.5fps \
+    #     Bellevue_Bellevue_NE8th__2017-09-11_09-08-30_0.5fps \
+    #     Bellevue_Bellevue_NE8th__2017-09-11_12-08-31_0.5fps \
+    #     Bellevue_Bellevue_NE8th__2017-09-11_15-08-32_0.5fps; do
+    for CITY in Bellevue_116th_NE12th__2017-09-10_19-08-25_0.5fps \
+        Bellevue_116th_NE12th__2017-09-10_22-08-50_0.5fps \
+        Bellevue_116th_NE12th__2017-09-11_01-08-29_0.5fps \
+        Bellevue_116th_NE12th__2017-09-11_04-08-30_0.5fps \
+        Bellevue_116th_NE12th__2017-09-11_07-08-32_0.5fps \
+        Bellevue_116th_NE12th__2017-09-11_11-08-33_0.5fps \
+        Bellevue_116th_NE12th__2017-09-11_15-08-36_0.5fps \
+        Bellevue_116th_NE12th__2017-09-11_16-08-37_0.5fps; do
     echo Running profiling on city $CITY Hparam: $HPARAM_ID
     CUDA_VISIBLE_DEVICES=3 python driver_multicity.py \
              --scheduler profiling \
              --cities ${CITY} \
-             --log-dir /data2/zxxia/ekya/results/${DATASET_NAME}_outputs/human_label_logs/ \
+             --log-dir ${RESULTS_ROOT}/${DATASET_NAME}_outputs/human_label_logs/ \
              --retraining-period ${RETRAINING_PERIOD} \
              --num-tasks ${NUM_TASKS} \
              --inference-chunks ${INFERENCE_CHUNKS} \
@@ -56,9 +81,9 @@ for HPARAM_ID in 0 1 2 3 4 5; do
              --inference-profile-path ${INFERENCE_PROFILE_PATH} \
              --max-inference-resources ${MAX_INFERENCE_RESOURCES} \
              --profiling-mode \
-             --profile-write-path /data2/zxxia/ekya/results/${DATASET_NAME}_outputs/human_label_profiles_sampled_${START_TASK}_${TERMINATION_TASK}/ \
+             --profile-write-path ${RESULTS_ROOT}/${DATASET_NAME}_outputs/human_label_profiles_sampled_${START_TASK}_${TERMINATION_TASK}/ \
              --golden-model-ckpt-path ${GOLDEN_MODEL_PATH} \
              --use-data-cache \
-             --checkpoint-path /data2/zxxia/ekya/results/${DATASET_NAME}_outputs/human_label_profiles_sampled_${START_TASK}_${TERMINATION_TASK}/
+             --checkpoint-path ${RESULTS_ROOT}/${DATASET_NAME}_outputs/human_label_profiles_sampled_${START_TASK}_${TERMINATION_TASK}/
   done
 done
